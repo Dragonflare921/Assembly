d42c2e959ea67ea6edab957b6368493bd427d817
Dragon <dragonflare921@gmail.com>
2020-01-27 16:28:42 -0800

    fixed cache version check for first gen

--- src/Blamite/Blam/CacheFileVersionInfo.cs
+++ src/Blamite/Blam/CacheFileVersionInfo.cs
@@ -1,4 +1,5 @@
 ﻿using Blamite.IO;
+using System;
 
 namespace Blamite.Blam
 {
@@ -7,6 +8,11 @@ namespace Blamite.Blam
 	/// </summary>
 	public class CacheFileVersionInfo
 	{
+        // first gen has a few different versions kicking around
+        private const int FirstGenXboxVersion = 5;
+        private const int FirstGenPCVersion = 7;// also CEA
+        private const int FirstGenCustomEditionVersion = 609;
+
 		private const int SecondGenVersion = 8;
 		private const int ThirdGenVersion = 9;
 
@@ -15,9 +21,37 @@ namespace Blamite.Blam
 			reader.SeekTo(0x4);
 			Version = reader.ReadInt32();
 
-			if (Version == SecondGenVersion)
+            if (Version == FirstGenPCVersion)
+            {
+                // first gen is all similar enough
+                Engine = EngineType.FirstGeneration;
+
+                // Read first-generation build string
+                reader.SeekTo(0x40);
+                BuildString = reader.ReadAscii();
+            }
+            else if (Version == FirstGenCustomEditionVersion)
+            {
+                // first gen is all similar enough
+                Engine = EngineType.FirstGeneration;
+
+                // Read first-generation build string
+                reader.SeekTo(0x40);
+                BuildString = reader.ReadAscii();
+            }
+            // TODO: support xbox
+            //       gonna need de/compression
+            else if (Version == FirstGenXboxVersion)
+            {
+                throw new NotImplementedException("assembly does not support loading first gen xbox maps (yet)");
+            }
+			else if (Version == SecondGenVersion)
 			{
 				Engine = EngineType.SecondGeneration;
+
+                // TODO: need to check if this is an xbox cache or PC cache
+                //       second gen uses the same version but different layout
+                //       which makes it a lil more annoying but still workable
 
 				// Read second-generation build string
 				reader.SeekTo(0x12C);
@@ -33,17 +67,6 @@ namespace Blamite.Blam
 				else
 					reader.SeekTo(0x120);
 
-				BuildString = reader.ReadAscii();
-			}
-
-			if (string.IsNullOrWhiteSpace(BuildString))
-			{
-				// Assume it's a first-generation build
-				Engine = EngineType.FirstGeneration;
-				Version = 0;
-
-				// Read first-generation build string
-				reader.SeekTo(0x40);
 				BuildString = reader.ReadAscii();
 			}
 		}
